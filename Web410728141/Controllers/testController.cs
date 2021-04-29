a5ef5bbc01d42f4f695e44dd3e77f7c867703b6c
蔡孟翰 <cena2023@gmail.com>
2021-04-29 20:28:30 +0800

    a

---
+++ Web410728141/Controllers/testController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410728141.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
